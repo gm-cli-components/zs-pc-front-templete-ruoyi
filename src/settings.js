27e42fbfb013dae3bc14f2babacbf576abd867fd
gaomeng <menggao965@163.com>
2022-03-29 15:49:54 +0800

    '*update'

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-29 15:48:19
+ * @LastEditTime: 2022-03-29 15:49:34
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \zs-pc-front-templete-ruoyi\src\settings.js
+ */
 module.exports = {
   /**
    * 侧边栏主题 深色主题theme-dark，浅色主题theme-light
    */
-  sideTheme: 'theme-dark',
+  sideTheme: 'theme-light',
 
   /**
    * 是否系统布局配置
